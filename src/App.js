4f4b1977e611fbb47d323fd6f4f983e5b5f4777f
Tiffany Hoang <tiffany.hoang@myob.com>
2021-11-16 19:27:29 +1100

    Add remove form and sort feature

--- src/App.js
+++ src/App.js
@@ -1,34 +1,40 @@
 import { useState } from 'react';
 import {nanoid} from 'nanoid';
-import { Form } from './components/Form';
+import { Form, FormRemove } from './components/Form';
 
-const Todo = ({ name }) => (<li>{name}</li>);
+const Todo = ({ name, priority, id }) => (<li>P: {priority}
+- N:{name} - Id: {id} </li>);
 
 const App = () => {
   const [tasks, setTasks] = useState([]);
 
-  const taskList = tasks.map(task => <Todo name={task.name} key={task.id} />)
+  const taskList = tasks.map(task => <Todo name={task.name} priority={task.priority} id={task.id} key={task.id} />)
 
-  const addTask = (name)=> {
-    const newTask = { id: "todo-" + nanoid(), name: name };
+  const addTask = (name, priority)=> {
+    const newTask = { id: "todo-" + nanoid(), name: name, priority: priority };
     setTasks([...tasks, newTask]);
   }
 
-  const removeTask = (name)=> {
-    const newTasks = tasks.filter(task => task.name !== name)
+  const removeTask = (id)=> {
+    const newTasks = tasks.filter(task => task.id !== id)
     setTasks([...newTasks]);
   }
 
+  const sort = () => {
+    const newTasks = tasks.sort((a,b)=> a.priority - b.priority);
+    setTasks([...newTasks]);
+  }
   return (
     <>
       <h1>Todo list</h1>
+      <button onClick={sort}>Sort</button>
       <ul>
         {taskList}
       </ul>
       <h2>What needs to be done?</h2>
       <Form onSubmit={addTask}/>
       <h2>What needs to be removed?</h2>
-      <Form onSubmit={removeTask}/>
+      <FormRemove onSubmit={removeTask}/>
     </>
   );
 }
--- src/components/Form.js
+++ src/components/Form.js
@@ -2,30 +2,77 @@ import { useState } from 'react';
 
 export const Form = ({onSubmit}) => {
     const [name, setName] = useState('');
+    const [priority, setPriority] = useState('');
 
     const handleChange = (e) => {
         setName(e.target.value);
     }
-  
+    const handleChangePriority = (e) => {
+        setPriority(e.target.value);
+    }
     const handleSubmit = (e) => {
         e.preventDefault();
-        onSubmit(name);
+        onSubmit(name, priority);
         setName("");
+        setPriority("");
     }
     
     return (
-        <form onSubmit={handleSubmit}>
-          <input
+        <form onSubmit={handleSubmit} >
+            <p>Task name</p>
+            <input
               type="text"
               name="text"
               autoComplete="off"
               value={name}
               onChange={handleChange}
-          />
-          <button type="submit">
-              Add
-          </button>
-      </form>
+            />
+            <p>Task priority</p>
+             <input
+              type="text"
+              name="text"
+              autoComplete="off"
+              value={priority}
+              onChange={handleChangePriority}
+            />
+            <button type="submit">
+                Add
+            </button>
+
+        </form>
+    )
+}
+
+
+
+
+export const FormRemove = ({onSubmit}) => {
+    const [id, setId] = useState('');
+
+    const handleChange = (e) => {
+        setId(e.target.value);
+    }
+    
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        onSubmit(id);
+        setId("");
+    }
+    
+    return (
+        <form onSubmit={handleSubmit} >
+            <p>Task Id</p>
+            <input
+              type="text"
+              name="text"
+              autoComplete="off"
+              value={id}
+              onChange={handleChange}
+            />
+            <button type="submit">
+                Remove
+            </button>
+        </form>
     )
 }
 
